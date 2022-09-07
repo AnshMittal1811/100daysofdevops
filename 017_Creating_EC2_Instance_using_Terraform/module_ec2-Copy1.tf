ccc1429e7cfad543b28e8ceb351d9fbd86cd7448
AnshMittal1811 <anshm18111996@gmail.com>
2022-09-07 14:54:06 -0700

    017 Creating EC2 Instance using Terraform

--- 017_Creating_EC2_Instance_using_Terraform/module_ec2-Copy1.tf
+++
@@ -1,9 +0,0 @@
-module "ec2_instance" {
-  source         = "./ec2_instance"
-  instance_count = "${var.instance_count}"
-  my_public_key  = "${var.my_public_key}"
-  instance_type  = "${var.instance_type}"
-  subnet_id      = "${module.vpc_networking.public_subnets}"
-  security_group = "${module.vpc_networking.security_group}"
-  alarm_actions  = "${module.sns.sns_topic}"
-}
