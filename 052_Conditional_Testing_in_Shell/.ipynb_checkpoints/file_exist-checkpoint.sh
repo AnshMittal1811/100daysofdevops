d72905063b20bed338c51e18d382fc3be2d8229d
AnshMittal1811 <anshm18111996@gmail.com>
2022-09-26 13:46:26 -0700

    052 Conditional Testing in Shell

--- 052_Conditional_Testing_in_Shell/.ipynb_checkpoints/file_exist-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILE="/etc/passwd"
-if [ -f $FILE ]; then
-   echo "The file '$FILE' exists."
-else
-   echo "The file '$FILE' in not found."
-fi
--- 052_Conditional_Testing_in_Shell/.ipynb_checkpoints/fileexits1-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILE="$1"
-if [ -f $FILE ]; then
-   echo "The file '$FILE' exists."
-else
-   echo "The file '$FILE' in not found."
-fi
